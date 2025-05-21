40d9dca20d7d0c55da99d861b82cfe97c88b1656
AurelioAlfons <yuroalfons0407@gmail.com>
2025-05-21 13:49:28 +1000

    first commit

---
+++ bin/smart_foot_traffic_frontend.dart
@@ -0,0 +1,5 @@
+import 'package:smart_foot_traffic_frontend/smart_foot_traffic_frontend.dart' as smart_foot_traffic_frontend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${smart_foot_traffic_frontend.calculate()}!');
+}
---
+++ test/smart_foot_traffic_frontend_test.dart
@@ -0,0 +1,8 @@
+import 'package:smart_foot_traffic_frontend/smart_foot_traffic_frontend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
